2301163f13dc62c646112710a2b0ef2783a5a26d
c9sid <kumarsiddharth7666@gmail.com>
2025-05-07 04:08:48 +0530

    Added customers section

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 const Brands = () => {
     return (
-        <div>Brands</div>
+        <section className='py-20'>
+            <div className='container mx-auto'>
+                <div>
+                    <h2 className='text-4xl font-bold text-center'>Trusted Brands</h2>
+                </div>
+                <marquee behavior="smooth" direction="left">
+                    <div className="brand flex gap-16 text-xl font-semibold py-10">
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Ray-Ban</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Titan Eye+</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Johnson & Johnson</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Bausch + Lomb</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Scott</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Freshlook</p>
+                        </div>
+                        <div className='rounded-lg bg-gray-100 p-5'>
+                            <p>Vogue</p>
+                        </div>
+                    </div>
+                </marquee>
+                <div>
+                    <h4 className='text-center'>All Top Brands Available at One Stop</h4>
+                </div>
+            </div>
+        </section>
     )
 }
 
