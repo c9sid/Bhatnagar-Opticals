1bd61310cdb8682d837244afdd3423a65d7b62cb
c9sid <kumarsiddharth7666@gmail.com>
2025-05-22 01:52:14 +0530

    Client Changes

--- components/Brands.jsx
+++ components/Brands.jsx
@@ -8,31 +8,27 @@ const Brands = () => {
                 <div>
                     <h2 className='text-2xl md:text-4xl font-bold text-center'>Trusted Brands</h2>
                 </div>
-                <marquee behavior="smooth" direction="left">
-                    <div className="brand flex gap-16 py-10">
-                        <div>
-                            <Image src={'/logo/Ray-Ban.png'} width={200} height={50} alt='Ray-Ban Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/tital-eye-plus.png'} width={200} height={50} alt='Titan Eye Plus Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/Johnson-and-Johnson.png'} width={200} height={50} alt='Johnson and Johnson Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/bausch-plus-lomb.png'} width={200} height={50} alt='Bausch Plus Lomb Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/scott.png'} width={200} height={50} alt='Scott Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/freshlook.png'} width={200} height={50} alt='Freshlook Logo' />
-                        </div>
-                        <div>
-                            <Image src={'/logo/vogue.png'} width={200} height={50} alt='Vogue Logo' />
-                        </div>
+                <div className="overflow-hidden py-10">
+                    <div className="flex animate-scroll gap-16 whitespace-nowrap">
+                        {[
+                            { src: "/logo/Ray-Ban.png", alt: "Ray-Ban Logo" },
+                            { src: "/logo/tital-eye-plus.png", alt: "Titan Eye Plus Logo" },
+                            { src: "/logo/Johnson-and-Johnson.png", alt: "Johnson and Johnson Logo" },
+                            { src: "/logo/bausch-plus-lomb.png", alt: "Bausch Plus Lomb Logo" },
+                            { src: "/logo/scott.png", alt: "Scott Logo" },
+                            { src: "/logo/freshlook.png", alt: "Freshlook Logo" },
+                            { src: "/logo/vogue.png", alt: "Vogue Logo" }
+                        ].map((logo, index) => (
+                            <img
+                                key={index}
+                                className="h-[40px] md:h-[50px] w-auto object-contain"
+                                src={logo.src}
+                                alt={logo.alt}
+                            />
+                        ))}
                     </div>
-                </marquee>
+                </div>
+
                 <div>
                     <h4 className='text-center text-sm md:text-base'>All Top Brands Available at One Stop</h4>
                 </div>
